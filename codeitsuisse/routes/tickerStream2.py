c8d76ab3b32d639c08cd5cccd3bf80da11fe303f
jaehwan-c <102342190+jaehwan-c@users.noreply.github.com>
2022-09-16 19:18:51 +0800

    Fix Bugs

--- codeitsuisse/routes/tickerStream2.py
+++ codeitsuisse/routes/tickerStream2.py
@@ -1,6 +1,5 @@
 import logging
 import json
-from this import d
 
 from flask import request, jsonify
 
@@ -8,17 +7,17 @@ from codeitsuisse import app
 
 logger = logging.getLogger(__name__)
 
-@app.route('/tickerStream2', methods=['POST'])
-def tickerStream2():
+@app.route('/tickerStreamPart1', methods=['POST'])
+def tickerStream1():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
-    quantity_block = data["quantityBlock"]
     stream = data["stream"]
-    result = to_cumulative_delayed(stream, quantity_block)
+    result = to_cumulative(stream)
     logging.info("My result :{}".format(result))
     return json.dumps(result)
 
-def to_cumulative_delayed(stream, quantity_block):
+def to_cumulative(stream):
+    
     result = {}
     for i in stream:
         i = i.split(',')
@@ -31,46 +30,30 @@ def to_cumulative_delayed(stream, quantity_block):
                 result[i[0]][i[1]][0] += float(i[2])
                 result[i[0]][i[1]][1] += float(i[2]) * float(i[3])
 
-    final_answer= {}
-    temp = []
-    prev_count = {}
+    final_answer = {}
+    tickers = set()
     for i in sorted(result):
-        
-        for j in sorted(result[i]):
-            if j not in final_answer:
-                count = 0
-                cumulative = 0
-                delayed_count = 0
-                delayed_cumulative = 0
-                delayed_i = i
+    
+        final_answer[i] = {}
+
+        for j in sorted(list(tickers)):
+            if j in result[i]:
+                final_answer[i][j] = [(final_answer[prev_i][j][0] + result[i][j][0]),(final_answer[prev_i][j][1] + result[i][j][1])]
             else:
-                count = final_answer[j][1]
-                cumulative = final_answer[j][2]
-                delayed_count = final_answer[j][4]
-                delayed_cumulative = final_answer[j][5]
-                delayed_i = final_answer[j][3]
-            for k in range(int(result[i][j][0])):
-                cumulative += (result[i][j][1] / result[i][j][0])
-                count += 1
-                if count % quantity_block == 0:
-                    delayed_count = count
-                    delayed_cumulative = cumulative
-                    delayed_i = i
-            final_answer[j] = [
-                i, count, round(cumulative,1), delayed_i, delayed_count,
-                round(delayed_cumulative,1)
-            ]
-        ans = [i]
-        for k in final_answer:
-            
-            cnt = final_answer[k][4]
-            cum = final_answer[k][5]
-            if k not in prev_count:
-                prev_count[k] = 0
-            if (cnt != 0) and (cnt%quantity_block==0) and (cnt > prev_count[k]):
-                ans += [k,str(cnt),str(cum)]
-                prev_count[k] = cnt
-        if len(ans) > 1:
-            temp.append(','.join(ans))
-    dict_to_return = {"output":temp}
+                final_answer[i][j] = [final_answer[prev_i][j][0],final_answer[prev_i][j][1] ]
+
+        for k in result[i]:
+            if k not in tickers:
+                final_answer[i][k] = [result[i][k][0], result[i][k][1],1]           
+                tickers.add(k)
+        prev_i = i       
+          
+    solution = []
+    for i in final_answer:
+        answer = [i]
+        for j in final_answer[i]:
+            answer += [j, str(int(final_answer[i][j][0])), str(round(final_answer[i][j][1],1))]
+        solution.append(','.join(answer)) 
+    dict_to_return = {"output":solution} 
+
     return dict_to_return
