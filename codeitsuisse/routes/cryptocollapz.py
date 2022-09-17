7bfd58f1b0d80d29df8ca53225a04f6067ec9ac8
jaehwan-c <102342190+jaehwan-c@users.noreply.github.com>
2022-09-17 10:30:56 +0800

    Update cryptocollapz.py

--- codeitsuisse/routes/cryptocollapz.py
+++ codeitsuisse/routes/cryptocollapz.py
@@ -1,5 +1,6 @@
 import logging
 import json
+from tkinter.tix import ExFileSelectBox
 
 from flask import request, jsonify
 
@@ -11,22 +12,30 @@ logger = logging.getLogger(__name__)
 def cryptocollapz():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
+    data = list(data)
     result = crypto_func(data)
     logging.info("My result :{}".format(result))
     return json.dumps(result)
 
 def crypto_func(data):
     lst_to_return = list()
-    for i in range(data):
+    for i in range(len(data)):
         temp_lst = list()
         for j in range(len(data[i])):
             temp_lst.append(max_value_checker(data[i][j]))
-        temp_lst.append(lst_to_return)
+        lst_to_return.append(temp_lst)
     
     return lst_to_return
 
 def max_value_checker(i):
-    while i % 2 == 0:
-        i = i // 2
+    index = i
+    lst = []
+    while index not in lst:
+        if index % 2 == 0:
+            lst.append(index)
+            index = index // 2
+        else:
+            lst.append(index)
+            index = index * 3 + 1
     else:
-        return i * 3 + 1
+        return max(lst)
