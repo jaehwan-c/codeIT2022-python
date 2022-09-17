0722c4c133a0d47a8a0e01aa1eda8bc47cab8eed
jaehwan-c <102342190+jaehwan-c@users.noreply.github.com>
2022-09-17 16:33:11 +0800

    Update rubiks.py

--- codeitsuisse/routes/rubiks.py
+++ codeitsuisse/routes/rubiks.py
@@ -45,45 +45,51 @@ def rubik_cube(ops, state):
     for i in range(len(action)):
         if "U" in action[i]:
             if "i" in action[i]: #Ui
-                final_state[1][0], final_state[2][0], final_state[3][0], final_state[4][0] = final_state[4][0], final_state[1][0], final_state[2][0], final_state[3][0]
+                for k in range(3):
+                    final_state[1][0], final_state[2][0], final_state[3][0], final_state[4][0] = final_state[2][0], final_state[3][0], final_state[4][0], final_state[1][0]
             else: #U
                 final_state[1][0], final_state[2][0], final_state[3][0], final_state[4][0] = final_state[2][0], final_state[3][0], final_state[4][0], final_state[1][0]
                 
         elif "L" in action[i]:
             if "i" in action[i]:
-                for j in range(3): #Li
-                    final_state[0][j][0], final_state[4][2-j][2], final_state[5][j][0], final_state[2][j][0] = final_state[2][j][0], final_state[0][j][0], final_state[4][2-j][2], final_state[5][j][0]
+                for k in range(3):
+                    for j in range(3): #L
+                        final_state[0][j][0], final_state[4][2-j][2], final_state[5][j][0], final_state[2][j][0] = final_state[4][2-j][2], final_state[5][j][0], final_state[2][j][0], final_state[0][j][0]
             else:
                 for j in range(3): #L
                     final_state[0][j][0], final_state[4][2-j][2], final_state[5][j][0], final_state[2][j][0] = final_state[4][2-j][2], final_state[5][j][0], final_state[2][j][0], final_state[0][j][0]
 
         elif "F" in action[i]:
             if "i" in action[i]: #Fi
-                for j in range(3):
-                    final_state[3][j][0], final_state[5][0][2-j], final_state[1][2-j][2], final_state[0][2][j] = final_state[5][0][2-j], final_state[1][2-j][2], final_state[0][2][j], final_state[3][j][0]
+                for k in range(3):
+                    for j in range(3):
+                        final_state[5][0][j], final_state[1][j][2], final_state[0][2][2-j], final_state[3][2-j][0] = final_state[3][2-j][0], final_state[5][0][j], final_state[1][j][2], final_state[0][2][j]
             else:
                 for j in range(3):
                     final_state[5][0][j], final_state[1][j][2], final_state[0][2][2-j], final_state[3][2-j][0] = final_state[3][2-j][0], final_state[5][0][j], final_state[1][j][2], final_state[0][2][j]
             
         elif "R" in action[i]:
             if "i" in action[i]:
-                for j in range(3): #Ri
-                    final_state[0][j][2], final_state[4][j][2], final_state[5][j][2], final_state[2][j][2] = final_state[2][j][2], final_state[0][j][2], final_state[4][j][2], final_state[5][j][2]
+                for k in range(3):
+                    for j in range(3): #R
+                        final_state[0][j][2], final_state[4][j][2], final_state[5][j][2], final_state[2][j][2] = final_state[4][j][2], final_state[5][j][2], final_state[2][j][2], final_state[0][j][2]
             else:
                 for j in range(3): #R
                     final_state[0][j][2], final_state[4][j][2], final_state[5][j][2], final_state[2][j][2] = final_state[4][j][2], final_state[5][j][2], final_state[2][j][2], final_state[0][j][2]
             
         elif "B" in action[i]:
             if "i" in action[i]: #Bi
-                for j in range(3):
-                    final_state[5][0][j], final_state[3][2-j][2], final_state[0][0][2-j], final_state[1][j][0] = final_state[3][2-j][2], final_state[0][0][2-j], final_state[1][j][0], final_state[5][0][j]
+                for k in range(3):
+                    for j in range(3):
+                        final_state[0][0][j], final_state[3][j][2], final_state[5][2][2-j], final_state[1][2-j][0] = final_state[3][j][2], final_state[5][2][2-j], final_state[1][2-j][0], final_state[0][0][j]           
             else: #B
                 for j in range(3):
                     final_state[0][0][j], final_state[3][j][2], final_state[5][2][2-j], final_state[1][2-j][0] = final_state[3][j][2], final_state[5][2][2-j], final_state[1][2-j][0], final_state[0][0][j]           
         
         else:
             if "i" in action[i]:             
-                final_state[1][2], final_state[2][2], final_state[3][2], final_state[4][2] = final_state[2][2], final_state[3][2], final_state[4][2], final_state[1][2]
+                for k in range(3):
+                    final_state[1][2], final_state[2][2], final_state[3][2], final_state[4][2] = final_state[4][2], final_state[1][2], final_state[2][2], final_state[3][2]
             else:
                 final_state[1][2], final_state[2][2], final_state[3][2], final_state[4][2] = final_state[4][2], final_state[1][2], final_state[2][2], final_state[3][2]
         
