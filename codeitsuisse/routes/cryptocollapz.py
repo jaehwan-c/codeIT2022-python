e734d8acea5eb4b42c4fedabe901f9ca9b67f846
jaehwan-c <102342190+jaehwan-c@users.noreply.github.com>
2022-09-17 11:37:35 +0800

    Update cryptocollapz.py

--- codeitsuisse/routes/cryptocollapz.py
+++ codeitsuisse/routes/cryptocollapz.py
@@ -18,15 +18,33 @@ def cryptocollapz():
 
 def crypto_func(data):
     lst_to_return = list()
+    dict_to_return = dict()
     for i in range(len(data)):
         temp_lst = list()
         for j in range(len(data[i])):
-            temp_lst.append(max_value_checker(data[i][j]))
+            lst = []    
+            num = data[i][j]
+            if i == 1 or i == 2:
+                return 4
+            else:
+                while num not in lst:
+                    if num % 2 == 0:
+                        lst.append(num)
+                        num = num // 2
+                        if num in dict_to_return.keys():
+                            temp_lst.append(dict_to_return[num])
+                    else:
+                        lst.append(num)
+                        num = 3 * num + 1
+                        if num in dict_to_return.keys():
+                            temp_lst.append(dict_to_return[num])
+                else:
+                    temp_lst.append(max(lst))
+            
         lst_to_return.append(temp_lst)
-    
     return lst_to_return
 
-def max_value_checker(i):
+def max_value_checker(i, dict_to_return):
     num = i
     lst = []
     
@@ -35,10 +53,11 @@ def max_value_checker(i):
     
     else:
         while num not in lst:
-
             if num % 2 == 0:
                 lst.append(num)
                 num = num // 2
+                if num in dict_to_return.keys():
+                    return dict_to_return[num]
             else:
                 lst.append(num)
                 num = 3 * num + 1
