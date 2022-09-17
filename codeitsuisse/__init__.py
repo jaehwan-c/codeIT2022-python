85199e17518b94b6953e34a2d5fc06882d352793
yeonpark <pyeon99@gmail.com>
2022-09-17 21:49:08 +0800

    update init

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -8,4 +8,5 @@ import codeitsuisse.routes.calendarDays
 import codeitsuisse.routes.reversle
 import codeitsuisse.routes.rubiks
 import codeitsuisse.routes.quordleKeyboard
-import codeitsuisse.routes.stig_warmu
+import codeitsuisse.routes.stig_warmup
+import codeitsuisse.routes.social_distancing
